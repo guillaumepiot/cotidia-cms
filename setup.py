ec114073cbeeaf355519ff9ba37ed560b7ffd5ca
Guillaume Piot <guillaume@cotidia.com>
2016-01-22 11:45:58 +0000

    Updated mptt version to 0.8.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
     'Django==1.9',
     'Pillow==2.9.0',
     'django-form-utils==1.0.3',
-    'django-mptt==0.7.4',
+    'django-mptt==0.8.0',
     'django-reversion==1.9.3',
     'djangorestframework==3.2.4',
     'django-codemirror-widget==0.4.0',
