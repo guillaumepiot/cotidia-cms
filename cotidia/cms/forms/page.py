84181d29ff795749b52463ebb6c6da574db5c923
Guillaume Piot <guillaume@cotidia.com>
2017-08-08 22:37:08 +0100

    Update page forms.

--- cotidia/cms/forms/page.py
+++ cotidia/cms/forms/page.py
@@ -6,6 +6,7 @@ from mptt.forms import TreeNodeChoiceField
 from cotidia.cms.models import Page, PageTranslation, PageDataSet
 from cotidia.cms.settings import CMS_PAGE_TEMPLATES
 
+
 class PageAddForm(forms.ModelForm):
 
     home = forms.BooleanField(
@@ -14,27 +15,41 @@ class PageAddForm(forms.ModelForm):
 
     display_title = forms.CharField(
         label='',
-        help_text=_("The display title is only used to represent the page "
+        help_text=_(
+            "The display title is only used to represent the page "
             "within the CMS. This value should not be used for the title "
-            "displayed on the web page."),
-        widget=forms.TextInput(attrs={'class':'form__text'})
-        )
+            "displayed on the web page."
+        ),
+        widget=forms.TextInput(attrs={'class': 'form__text'})
+    )
 
     template = forms.ChoiceField(
         label='',
-        help_text=_("The template defines the layout of page, as well as the "
-            "editable areas."),
+        help_text=_(
+            "The template defines the layout of page, as well as the "
+            "editable areas."
+        ),
         choices=CMS_PAGE_TEMPLATES,
-        widget=forms.Select(attrs={'class':'form__select'})
-        )
+        widget=forms.Select(attrs={'class': 'form__select'})
+    )
+
+    parent = forms.ModelChoiceField(
+        label='',
+        help_text=("Leave blank if this a top level page."),
+        queryset=Page.objects.get_originals(),
+        widget=forms.Select(attrs={'class': 'form__select'}),
+        required=False
+    )
 
     hide_from_nav = forms.BooleanField(
         label=_("Hide from menu"),
-        help_text=_("Check this box if you want to hide this page "
-            "from the main menu"),
-        widget=forms.CheckboxInput(attrs={'class':'form__checkbox'}),
+        help_text=_(
+            "Check this box if you want to hide this page "
+            "from the main menu"
+        ),
+        widget=forms.CheckboxInput(attrs={'class': 'form__checkbox'}),
         required=False
-        )
+    )
 
     class Meta:
         model = Page
@@ -44,16 +59,7 @@ class PageAddForm(forms.ModelForm):
             'template',
             'parent',
             'hide_from_nav'
-            ]
-
-
-    def __init__(self, *args, **kwargs):
-        super(PageAddForm, self).__init__(*args, **kwargs)
-        self.fields['parent'].label = ""
-        self.fields['parent'].widget.attrs['class'] = 'form__text'
-        self.fields['parent'].help_text = _("Leave blank if this a top level page.")
-        self.fields['parent'].queryset = Page.objects.get_originals()
-
+        ]
 
     def clean_home(self):
         home = self.cleaned_data['home']
@@ -66,47 +72,48 @@ class PageAddForm(forms.ModelForm):
             if self.instance:
                 if self.Meta.model.objects.filter(
                     published_from=None,
-                    home=True).exclude(id=self.instance.id):
+                    home=True
+                ).exclude(id=self.instance.id):
                     raise forms.ValidationError(err_message)
             # Check if other pages are already home excluded
             else:
                 if self.Meta.model.objects.filter(
-                    published_from=None, home=True):
+                    published_from=None, home=True
+                ):
                     raise forms.ValidationError(err_message)
 
         return home
 
-class PageUpdateForm(PageAddForm):
 
+class PageUpdateForm(PageAddForm):
 
     slug = forms.CharField(
         label='',
         help_text=_("A unique identifier to allow retrieving a page"),
-        widget=forms.TextInput(attrs={'class':'form__text'}),
+        widget=forms.TextInput(attrs={'class': 'form__text'}),
         required=False
-        )
+    )
 
     dataset = forms.ModelChoiceField(
         label="",
         queryset=PageDataSet.objects.all(),
-        widget=forms.Select(attrs={'class':'form__select'}),
+        widget=forms.Select(attrs={'class': 'form__select'}),
         required=False
-        )
+    )
 
     redirect_to = TreeNodeChoiceField(
         label='',
         queryset=Page.objects.get_published_originals(),
         help_text=_('Redirect this page to another page in the system'),
-        widget=forms.Select(attrs={'class':'form__select'}),
+        widget=forms.Select(attrs={'class': 'form__select'}),
         required=False)
-
 
     redirect_to_url = forms.CharField(
         label='',
         help_text=_("Enter the full web address."),
-        widget=forms.TextInput(attrs={'class':'form__text'}),
+        widget=forms.TextInput(attrs={'class': 'form__text'}),
         required=False
-        )
+    )
 
     class Meta:
         model = Page
@@ -120,25 +127,28 @@ class PageUpdateForm(PageAddForm):
             'redirect_to',
             'redirect_to_url',
             'slug'
-            ]
+        ]
+
 
 class PageURLForm(forms.ModelForm):
 
     slug = forms.CharField(
         label='',
-        help_text=_("The URL should only contain lowercase letters, "
-            "numbers and dashes (-)."),
+        help_text=_(
+            "The URL should only contain lowercase letters, "
+            "numbers and dashes (-)."
+        ),
         widget=forms.TextInput(attrs={
-            'class':'form__text',
-            'onKeyUp':'updateSlug(this)'
-            })
-        )
+            'class': 'form__text',
+            'onKeyUp': 'updateSlug(this)'
+        })
+    )
 
     class Meta:
         model = PageTranslation
         fields = [
             'slug'
-            ]
+        ]
 
     def clean_slug(self):
 
@@ -150,18 +160,20 @@ class PageURLForm(forms.ModelForm):
         slug_pattern = re.compile("^([a-z0-9\-]+)$")
 
         if not slug_pattern.match(slug):
-            raise forms.ValidationError(_("The URL is not valid "
-                "because it contains unallowed characters. "
-                "Only lowercase letters, numbers and dashes are accepted."))
-
-
-
+            raise forms.ValidationError(
+                _(
+                    "The URL is not valid "
+                    "because it contains unallowed characters. "
+                    "Only lowercase letters, numbers and dashes are accepted."
+                )
+            )
 
         pages = [page.slug for page in PageTranslation.objects.all()]
 
         error_message = _('The unique page identifier must be unique')
 
-        if self.instance and slug in pages and slug != self.instance.slug and slug != '':
+        if self.instance and slug in pages \
+                and slug != self.instance.slug and slug != '':
             raise forms.ValidationError(error_message)
 
         elif not self.instance and slug in pages and slug != '':
@@ -170,16 +182,17 @@ class PageURLForm(forms.ModelForm):
         else:
             return slug
 
+
 class PageTitleForm(forms.ModelForm):
 
     title = forms.CharField(
         label='',
         help_text=_("The title will be used for navigation items."),
-        widget=forms.TextInput(attrs={'class':'form__text'})
-        )
+        widget=forms.TextInput(attrs={'class': 'form__text'})
+    )
 
     class Meta:
         model = PageTranslation
         fields = [
             'title'
-            ]
+        ]
